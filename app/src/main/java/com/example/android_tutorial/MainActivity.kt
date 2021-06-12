3521cd24a58a4922c5a589884632f37318a2878b
thonghkk <nguyenvanthonghkk@gmail.com>
2021-06-13 01:33:06 +0700

    Handling Save and Restore Instance state

--- app/src/main/java/com/example/android_tutorial/MainActivity.kt
+++ app/src/main/java/com/example/android_tutorial/MainActivity.kt
@@ -2,10 +2,40 @@ package com.example.android_tutorial
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mTxt: TextView
+    private lateinit var mBtn: Button
+    private var selection: Int = 0
+        set(value) {
+            field = value
+            mTxt.text = field.toString()
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initView()
+
+        mBtn.setOnClickListener {
+            selection++
+        }
+    }
+
+    private fun initView() {
+        mTxt = findViewById(R.id.mTxtCount)
+        mBtn = findViewById(R.id.mBtnClick)
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt("select", selection)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        selection = savedInstanceState.getInt("select")
     }
 }
